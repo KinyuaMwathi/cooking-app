52f5c6e204d245a39365d18a6c4adf73ae6d0717
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-04-19 13:32:55 +0300

    Add the user interface logic for the form and the jQuery library

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,38 @@
-function convertLitres(number) {
-  return number * 1000;
+function convertMillilitres(number) {
+  return number / 1000;
 }
 
-function convertKgs(number) {
-  return number * 1000;
+function convertGrams(number) {
+  return number / 1000;
 }
 
-function convertTablespoon(number) {
+function convertTeaspoons(number) {
   return number / 3;
 }
 
-function convertTempFahrenheit(number) {
-  return (number * 9 / 5) + 32 + " " + "degrees fahrenheit";
+function convertTempCelsius(number) {
+  return (number * 9 / 5) + 32;
 }
+
+
+$(document).ready(function() {
+  $("form#converterForm").submit(function(event) {
+    event.preventDefault();
+    const number = parseInt($("#number").val());
+    const unitConverter = $("input:radio[name=unitConverter]:checked").val();
+    let result;
+
+    if(unitConverter === 'convert millilitres to litres') {
+      result = convertMillilitres(number);
+    } else if(unitConverter === 'convert grams to kilograms') {
+      result = convertGrams(number);
+    } else if(unitConverter === 'convert teaspoons to tablespoons') {
+      result = convertTeaspoons(number);
+    } else if(unitConverter === 'convert degrees celsius to degrees fahrenheit') {
+      result = convertTempCelsius(number);
+    }
+
+    $("#output").text(result);
+    $("#converterResult").show();
+  });
+});
