a7d11c839f43b430a30424c4d9ccb55982a864af
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-04-19 11:09:13 +0300

    Add business logic scripts

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,10 @@ function convertKgs(number) {
   return number * 1000;
 }
 
-function convertTeaspoon(number) {
+function convertTablespoon(number) {
   return number / 3;
 }
 
-function convertTempInCelsius(number) {
+function convertTempFahrenheit(number) {
   return (number * 9 / 5) + 32 + " " + "degrees fahrenheit";
 }
-
-const number = parseInt(prompt("Enter a number"));
-
-alert(convertTempInCelsius(number));
